9baae2431713fc0fd9ff2fe8e2ff682278c749b0
Rushabh <patrerushabh2468@gmail.com>
2022-02-17 09:57:06 +0530

    Add project files.

---
+++ LiveSports/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using LiveSports.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiveSports.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult News()
+        {
+            return View();
+        }
+        public IActionResult Membership()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult InternationalCricket()
+        {
+            return View();
+        }
+        public IActionResult Football()
+        {
+            return View();
+        }
+        public IActionResult Aboutus()
+        {
+            return View(); 
+        }
+        //public IActionResult Domestic()
+        //{
+        //    return View();
+        //}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
